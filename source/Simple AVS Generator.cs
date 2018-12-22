e49767763fa8a7a1717acb9bd11ff5fd21cf20e9
Shaylen <shaylenreddy42@gmail.com>
2018-12-22 19:24:30 +0200

    Add Two More Selectable Audio Bitrates

--- source/Simple AVS Generator.cs
+++ source/Simple AVS Generator.cs
@@ -68,28 +68,28 @@ namespace Simple_AVS_Generator
          * 3rd Dimension: List of sane bitrates
          */
         int [,,] selectableAudioBitrates =
+        {
+            //AAC-LC
             {
-                //AAC-LC
-                {
-                    { 112, 128, 144, 160 }, //2 Channels
-                    { 256, 288, 320, 384 }, //5.1 Channels
-                    { 448, 512, 576, 640 }  //7.1 Channels
-                },
+                {  96, 112, 128, 144, 160, 192 }, //2 Channels
+                { 192, 224, 256, 288, 320, 384 }, //5.1 Channels
+                { 384, 448, 512, 576, 640, 768 }  //7.1 Channels
+            },
 
-                //AAC-HE
-                {
-                    {  48,  56,  64,  80 }, //2 Channels
-                    { 112, 128, 160, 192 }, //5.1 Channels
-                    { 160, 192, 224, 256 }  //7.1 Channels
-                },
+            //AAC-HE
+            {
+                {  32,  40,  48,  56,  64,  80 }, //2 Channels
+                {  80,  96, 112, 128, 160, 192 }, //5.1 Channels
+                { 112, 128, 160, 192, 224, 256 }  //7.1 Channels
+            },
 
-                //OPUS
-                {
-                    {  96, 112, 128, 144 }, //2 Channels
-                    { 192, 224, 256, 288 }, //5.1 Channels
-                    { 288, 320, 384, 448 }  //7.1 Channels
-                }
-            };
+            //OPUS
+            {
+                {  96, 112, 128, 144, 160, 192 }, //2 Channels
+                { 144, 160, 192, 224, 256, 288 }, //5.1 Channels
+                { 256, 288, 320, 384, 448, 576 }  //7.1 Channels
+            }
+        };
         
         /**
          * 1st Dimension: Codec [AAC-LC, AAC-HE, OPUS]
