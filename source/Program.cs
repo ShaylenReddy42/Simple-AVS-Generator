7b2e9eada35986faad5d11cb765742445fe738aa
Shaylen <shaylenreddy42@gmail.com>
2020-01-12 11:53:38 +0200

    update copyright to 2020

--- source/Program.cs
+++ source/Program.cs
@@ -1,5 +1,5 @@
 ﻿/******************************************************************************
- * Copyright (C) 2018-2019 Shaylen Reddy
+ * Copyright (C) 2018-2020 Shaylen Reddy
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
