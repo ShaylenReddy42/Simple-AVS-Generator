3e7029cf5fa41021a14b6b38e3440adc574b5cd7
Shaylen <shaylenreddy42@gmail.com>
2021-07-11 16:29:09 +0200

    remove --ssim from x264 presets
    serves no purpose unless tune ssim is used which i won't do

--- source/Simple AVS Generator.cs
+++ source/Simple AVS Generator.cs
@@ -322,13 +322,13 @@ namespace Simple_AVS_Generator
                 else if (cmbVideoCodec.SelectedIndex == (int) Video.AVC)
                 {
                     vEncoder += "x264 --preset veryslow --crf 26 -i 1 -I 48 --bframes 3 --deblock -2:-1 --aq-mode 3 ";
-                    vEncoder += "--ssim --aud --no-mbtree --demuxer y4m --frames 0 -o \"%~dp0Video.264\" -";
+                    vEncoder += "--aud --no-mbtree --demuxer y4m --frames 0 -o \"%~dp0Video.264\" -";
                     vCmdFile += "Encode Video [AVC].cmd";
                 }
                 else if (cmbVideoCodec.SelectedIndex == (int) Video.WhatsApp)
                 {
                     vEncoder += "x264 --profile baseline --preset veryslow --crf 26 -i 1 --ref 1 --deblock -2:-1 ";
-                    vEncoder += "--ssim --aud --no-mbtree --demuxer y4m --frames 0 -o \"%~dp0Video.264\" -";
+                    vEncoder += "--aud --no-mbtree --demuxer y4m --frames 0 -o \"%~dp0Video.264\" -";
                     vCmdFile += "Encode Video [AVC].cmd";
                 }
 
