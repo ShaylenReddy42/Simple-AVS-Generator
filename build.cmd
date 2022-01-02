c2a6ccd3e7acac366428a3b4f0801b1254994331
Shaylen <shaylenreddy42@gmail.com>
2022-01-02 10:56:13 +0200

    upgrade target .net framework to 4.8 to be supported by vs2022
    after upgrading from vs2019 to vs2022, builds were failing due to the framework being too old. the only version supported seemed to be 4.8 after various testing
    
    this means that windows xp and vista support is removed with targeting this framework version with windows 7 x64 being the oldest platform supported
    
    also upgrade the minimum cmake version to 3.22 to support the visual studio 17 generator

--- build.cmd
+++ build.cmd
@@ -1,25 +1,25 @@
 @ECHO off
 
-IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" (
+IF EXIST "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" (
+	@ECHO Found Visual Studio 2022
+	@ECHO.
+	@CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+	@ECHO.
+	SET GENERATOR=Visual Studio 17
+) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" ( 
 	@ECHO Found Visual Studio 2019
 	@ECHO.
 	@CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 	@ECHO.
 	SET GENERATOR=Visual Studio 16
-) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" ( 
-	@ECHO Found Visual Studio 2017
-	@ECHO.
-	@CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-	@ECHO.
-	SET GENERATOR=Visual Studio 15 Win64
 ) ELSE (
-	@ECHO Visual Studio 2019 nor 2017 could be found
+	@ECHO Visual Studio 2022 nor 2019 could be found
 	@GOTO END
 )
 
 @MD "%~dp0build"
 
-cmake -G "%GENERATOR%" -S "%~dp0source" -B "%~dp0build"
+cmake -G "%GENERATOR%" -A "x64" -S "%~dp0source" -B "%~dp0build"
 @ECHO.
 
 IF EXIST "%~dp0build\SimpleAVSGenerator.sln" ( 
