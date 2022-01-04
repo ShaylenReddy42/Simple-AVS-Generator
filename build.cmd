45815b90d081eb7463c99f1dad0e648c7b0de85d
Shaylen <shaylenreddy42@gmail.com>
2022-01-04 11:26:04 +0200

    build: remove redundancy from the script
    * remove call to the vsdevcmd.bat as it is no longer used. dotnet is used instead
    * reintroduce the pause in the end only if the version.txt file doesn't exist which is created when building with appveyor
    * fix duplicate cmake call when building from appveyor

--- build.cmd
+++ build.cmd
@@ -1,25 +1,20 @@
 @ECHO off
 
-IF EXIST "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" (
-	@ECHO Found Visual Studio 2022
-	@ECHO.
-	@CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-	@ECHO.
-	SET GENERATOR=Visual Studio 17
-) ELSE (
-	@ECHO Visual Studio 2022 could not be found
-	@GOTO END
-)
-
 @MD "%~dp0build"
 
-cmake -G "%GENERATOR%" -S . -B "%~dp0build"
-@ECHO.
+IF NOT EXIST "%~dp0version.txt" (
+	cmake -G "Visual Studio 17" -S . -B "%~dp0build"
+	@ECHO.
+)
 
-@CALL dotnet-publish.cmd
+@CALL "%~dp0dotnet-publish.cmd"
 @ECHO.
 
 @RD "%~dp0bin" "%~dp0build" "%~dp0obj" /S /Q
 @DEL "%~dp0dotnet-publish.cmd"
 
 :END
+
+IF NOT EXIST "%~dp0version.txt" (
+	@PAUSE
+)
