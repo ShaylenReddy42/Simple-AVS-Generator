697b42ba577fb5d84711334457a02781b79c9d58
Shaylen <shaylenreddy42@gmail.com>
2022-02-14 18:43:45 +0200

    tests: add pattern matching tests for the extensions class
    since the methods in this class are private, the only way to test it was using the properties of the class to check the pattern
    
    it was a little difficult to get the patterns right but now it works and this completes the testing for this class

--- Simple AVS Generator.Core.Tests/Support/ExtensionsTests.cs
+++ Simple AVS Generator.Core.Tests/Support/ExtensionsTests.cs
@@ -38,10 +38,10 @@ namespace Simple_AVS_Generator.Core.Tests.Support
         public static IEnumerable<object[]> IsSupportedByMP4Box_ValidateSupport_TestData =
         new[]
         {
-            new object[]{ ".mp4", true  },
-            new object[]{ ".mkv", false },
-            new object[]{ ".264", true  },
-            new object[]{ ".m4a", true  },
+            new object[] { ".mp4", true  },
+            new object[] { ".mkv", false },
+            new object[] { ".264", true  },
+            new object[] { ".m4a", true  },
         };
 
         [Theory (DisplayName = "Validate That MP4Box Support Is Set Correctly")]
@@ -61,6 +61,105 @@ namespace Simple_AVS_Generator.Core.Tests.Support
 
             // Assert
             Assert.Equal(expectedIsSupportedByMP4Box, actualIsSupportedByMP4Box);
+        }
+
+        [Theory (DisplayName = "Check Supported Extension Type Strings For Regex Pattern \"*.ext1;*.ext2\"")]
+        [InlineData((int)ExtensionTypes.CONTAINER)]
+        [InlineData((int)ExtensionTypes.VIDEO)]
+        [InlineData((int)ExtensionTypes.AUDIO)]
+        public void SupportedExtensionType_CheckPatternForMatch(int extensionType)
+        {
+            // Arrange
+            // This pattern was a little difficult to set up
+            // So lets break it down
+            // The string must look like this "*.ext1;*.ext2;*.ext3" without the ';' at the end
+            // A grouping can be seen, *.ext1 being a group
+            // In regex, a group is inside round brackets ()
+            // The * and the . are special characters in regex
+            // but couldn't be escaped by the normal means e.g. \. \*
+            // so it had to be in square brackets in the group
+            // The pattern so far is ([*][.])
+            // The next grouping is the characters that make the rest of the regex group,
+            // alphanumeric characters a to z and 0 to 9 which is together [a-z0-9]
+            // ([*][.][a-z0-9])
+            // In the group, it ends in a semi-colon
+            // The complete group ([*][.][a-z0-9];)
+            // It appears continuously throughout the string so use the * to indicate that
+            // ([*][.][a-z0-9];)*
+            // Now the string cannot end with the semi-colon but that has to be in square brackets [;]
+            // To ensure it doesn't, you have to use this character ^ to negate it [^;]
+            // ([*][.][a-z0-9];)*[^;]
+            // And finally, the end of string character $
+            string expectedPattern = "([*][.][a-z0-9];)*[^;]$";
+
+            // Act
+            Extensions se = new();
+            string? supportedExts = "";
+
+            switch (extensionType)
+            {
+                case (int)ExtensionTypes.CONTAINER:
+                    supportedExts = se.SupportedContainerExts;
+                    break;
+                case (int)ExtensionTypes.VIDEO:
+                    supportedExts = se.SupportedVideoExts;
+                    break;
+                case (int)ExtensionTypes.AUDIO:
+                    supportedExts = se.SupportedAudioExts;
+                    break;
+            }
+
+            // Assert
+            Assert.Matches(expectedPattern, supportedExts);
+        }
+
+        [Theory(DisplayName = "Check Filter Extension Type Strings For Regex Pattern \"EXT1 EXT2\"")]
+        [InlineData((int)ExtensionTypes.CONTAINER)]
+        [InlineData((int)ExtensionTypes.VIDEO)]
+        [InlineData((int)ExtensionTypes.AUDIO)]
+        public void FilterExtensionType_CheckPatternForMatch(int extensionType)
+        {
+            // Arrange
+            // This pattern was a little similar to the previous but with a new challenge
+            // \w is not the escape character for a whitespace
+            // so I had to consult the documentation and found this \x020
+            // This of all things is the escape character for a whitespace
+            // So lets break it down
+            // The string must look like this "EXT1 EXT2 EXT3" without the whitespace at the end
+            // A grouping can be seen, 'EXT1\x020' being a group
+            // In regex, a group is inside round brackets ()
+            // The regex group consists of alphanumeric characters A to Z and 0 to 9
+            // which is together in square brackets [A-Z0-9]
+            // ([A-Z0-9])
+            // In the regex group, it ends in a whitespace
+            // The complete group ([A-Z0-9]\x020)
+            // It appears continuously throughout the string so use the * to indicate that
+            // ([A-Z0-9]\x020)*
+            // Now the string cannot end with a whitespace but that has to be in square brackets [\x020]
+            // To ensure it doesn't, you have to use this character ^ to negate it [^\x020]
+            // ([A-Z0-9]\x020)*[^\x020]
+            // And finally, the end of string character $
+            string expectedPattern = "([A-Z0-9]\x020)*[^\x020]$";
+
+            // Act
+            Extensions se = new();
+            string? filterExts = "";
+
+            switch (extensionType)
+            {
+                case (int)ExtensionTypes.CONTAINER:
+                    filterExts = se.FilterContainerExts;
+                    break;
+                case (int)ExtensionTypes.VIDEO:
+                    filterExts = se.FilterVideoExts;
+                    break;
+                case (int)ExtensionTypes.AUDIO:
+                    filterExts = se.FilterAudioExts;
+                    break;
+            }
+
+            // Assert
+            Assert.Matches(expectedPattern, filterExts);
         }
     }
 }
