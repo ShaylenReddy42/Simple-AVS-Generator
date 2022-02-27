33cb1a11f1097f369ce9c3113695bb15cdcc57b8
Shaylen <shaylenreddy42@gmail.com>
2022-02-27 17:26:37 +0200

    cleanup the OutputScripts' methods

--- SimpleAVSGeneratorCore/OutputScripts.cs
+++ SimpleAVSGeneratorCore/OutputScripts.cs
@@ -43,75 +43,65 @@ public class OutputScripts
         if (_common.Video is true && _common.MuxOriginalVideo is false)
         {
             string? vPipe = "avs2pipemod -y4mp \"%~dp0Script.avs\" | ",
-                    vEncoder = "",
-                    vCmdFile = _common.OutputDir;
+                    vEncoder = "";
 
             if (_common.VideoCodec == "HEVC")
             {
                 vEncoder += $"x265 --profile main --preset slower --crf 26 -i 1 -I {GetKeyframeIntervalInFrames()} --hist-scenecut --hist-threshold 0.02 ";
                 vEncoder += "--fades --aq-mode 4 --aq-motion --aud --no-open-gop --y4m -f 0 - \"%~dp0Video.265\"";
-                vCmdFile += "Encode Video [HEVC].cmd";
             }
             else if (_common.VideoCodec == "AV1")
             {
                 vEncoder += "aomenc --passes=1 --end-usage=q --cq-level=32 --target-bitrate=0 ";
                 vEncoder += $"--enable-fwd-kf=1 --kf-max-dist={GetKeyframeIntervalInFrames()} --verbose --ivf -o \"%~dp0Video.ivf\" -";
-                vCmdFile += "Encode Video [AV1].cmd";
             }
             else if (_common.VideoCodec == "AVC")
             {
                 vEncoder += $"x264 --preset veryslow --crf 26 -i 1 -I {GetKeyframeIntervalInFrames()} --bframes 3 --deblock -2:-1 --aq-mode 3 ";
                 vEncoder += "--aud --no-mbtree --demuxer y4m --frames 0 -o \"%~dp0Video.264\" -";
-                vCmdFile += "Encode Video [AVC].cmd";
             }
             else if (_common.VideoCodec == "WhatsApp")
             {
                 vEncoder += $"x264 --profile baseline --preset veryslow --crf 26 -i 1 -I {GetKeyframeIntervalInFrames()} --ref 1 --deblock -2:-1 ";
                 vEncoder += "--aud --no-mbtree --demuxer y4m --frames 0 -o \"%~dp0Video.264\" -";
-                vCmdFile += "Encode Video [WhatsApp].cmd";
             }
 
-            VideoEncoderScriptFile = vCmdFile;
+            VideoEncoderScriptFile = $"{_common.OutputDir}Encode Video [{_common.VideoCodec}].cmd";
             VideoEncoderScriptContent = vPipe + vEncoder;
         }
     }
 
     public void ConfigureAudioScript()
     {
-        if (_common.Audio)
+        if (_common.Audio is true)
         {
             string? aPipe = "avs2pipemod -wav=16bit \"%~dp0Script.avs\" | ",
-                    aEncoder = "",
-                    aCmdFile = _common.OutputDir;
+                    aEncoder = "";
 
             if (_common.AudioCodec == "AAC-LC")
             {
                 aEncoder += $"qaac64 --abr {_common.AudioBitrate} --ignorelength --no-delay ";
                 aEncoder += $"-o \"%~dp0{_common.FileNameOnly}{_common.AudioExtension}\" - ";
-                aCmdFile += "Encode Audio [AAC-LC].cmd";
             }
             else if (_common.AudioCodec == "AAC-HE")
             {
                 aEncoder += $"qaac64 --he --abr {_common.AudioBitrate} --ignorelength ";
                 aEncoder += $"-o \"%~dp0{_common.FileNameOnly}{_common.AudioExtension}\" - ";
-                aCmdFile += "Encode Audio [AAC-HE].cmd";
             }
-            else //OPUS
+            else if (_common.AudioCodec == "OPUS")
             {
                 aEncoder += $"opusenc --bitrate {_common.AudioBitrate} --ignorelength ";
                 aEncoder += $"- \"%~dp0{_common.FileNameOnly}{_common.AudioExtension}\"";
-                aCmdFile += "Encode Audio [OPUS].cmd";
             }
 
-            AudioEncoderScriptFile = aCmdFile;
+            AudioEncoderScriptFile = $"{_common.OutputDir}Encode Audio [{_common.AudioCodec}].cmd";
             AudioEncoderScriptContent = aPipe + aEncoder;
         }
     }
 
     public void ConfigureContainerScript()
     {
-        string? outputFileName = _common.OutputDir,
-                fileContents = null;
+        string? fileContents = null;
 
         if (_common.OutputContainer == "MP4")
         {
@@ -121,7 +111,6 @@ public class OutputScripts
                    mp4A = _common.Audio ? $"-add \"%~dp0{_common.FileNameOnly}{_common.AudioExtension}\":name=:lang={_common.AudioLanguage}" : "",
                    newmp4 = $"-new \"%~dp0{_common.FileNameOnly}.mp4\"";
 
-            outputFileName += $"MP4 Mux{(_common.MuxOriginalVideo ? " [Original Video]" : "")}.cmd";
             fileContents = $"mp4box {mp4V} {mp4A} {newmp4}";
         }
         else if (_common.OutputContainer == "MKV")
@@ -132,11 +121,12 @@ public class OutputScripts
                         : $"--no-audio \"{_common.FileName}\"",
                    mkvA = _common.Audio ? $"--language 0:{_common.AudioLanguage} \"%~dp0{_common.FileNameOnly}{_common.AudioExtension}\"" : "";
 
-            outputFileName += $"MKV Mux{(_common.MuxOriginalVideo ? " [Original Video]" : "")}.cmd";
             fileContents = $"mkvmerge {mkvO} {mkvV} {mkvA}";
         }
 
-        ContainerScriptFile = outputFileName;
+        ContainerScriptFile = _common.OutputContainer is not null
+                            ? $"{_common.OutputDir}{_common.OutputContainer} Mux{(_common.MuxOriginalVideo ? " [Original Video]" : "")}.cmd"
+                            : null;
         ContainerScriptContent = fileContents;
     }
 }
