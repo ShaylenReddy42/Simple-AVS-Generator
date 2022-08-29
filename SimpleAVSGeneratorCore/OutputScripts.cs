fb4b135008d9231eb8428c00256a67c64fb5bbdd
Shaylen <shaylenreddy42@gmail.com>
2022-08-29 23:09:59 +0200

    use verbatim strings to improve readability in the outputscripts class

--- SimpleAVSGeneratorCore/OutputScripts.cs
+++ SimpleAVSGeneratorCore/OutputScripts.cs
@@ -19,28 +19,28 @@ public class OutputScripts
     {
         if (video.Enabled is true && video.MuxOriginalVideo is false)
         {
-            string vPipe    = "avs2pipemod -y4mp \"%~dp0Script.avs\" | ",
+            string vPipe    = @"avs2pipemod -y4mp ""%~dp0Script.avs"" | ",
                    vEncoder = string.Empty;
 
             if (video.Codec is "HEVC")
             {
                 vEncoder += $"x265 --profile main --preset slower --crf 26 -i 1 -I {video.KeyframeIntervalInFrames} --hist-scenecut --hist-threshold 0.02 ";
-                vEncoder += $"--fades --aq-mode 4 --aq-motion --aud --no-open-gop --y4m --frames {video.SourceFrameCount} - \"%~dp0Video{video.Extension}\"";
+                vEncoder += $@"--fades --aq-mode 4 --aq-motion --aud --no-open-gop --y4m --frames {video.SourceFrameCount} - ""%~dp0Video{video.Extension}""";
             }
             else if (video.Codec is "AV1")
             {
                 vEncoder += "aomenc --passes=1 --end-usage=q --cq-level=32 --target-bitrate=0 ";
-                vEncoder += $"--enable-fwd-kf=1 --kf-max-dist={video.KeyframeIntervalInFrames} --verbose --ivf -o \"%~dp0Video{video.Extension}\" -";
+                vEncoder += $@"--enable-fwd-kf=1 --kf-max-dist={video.KeyframeIntervalInFrames} --verbose --ivf -o ""%~dp0Video{video.Extension}"" -";
             }
             else if (video.Codec is "AVC")
             {
                 vEncoder += $"x264 --preset veryslow --crf 26 -i 1 -I {video.KeyframeIntervalInFrames} --bframes 3 --deblock -2:-1 --aq-mode 3 ";
-                vEncoder += $"--aud --no-mbtree --demuxer y4m --frames {video.SourceFrameCount} -o \"%~dp0Video{video.Extension}\" -";
+                vEncoder += $@"--aud --no-mbtree --demuxer y4m --frames {video.SourceFrameCount} -o ""%~dp0Video{video.Extension}"" -";
             }
             else if (video.Codec is "WhatsApp")
             {
                 vEncoder += $"x264 --profile baseline --preset veryslow --crf 26 -i 1 -I {video.KeyframeIntervalInFrames} --ref 1 --deblock -2:-1 ";
-                vEncoder += $"--aud --no-mbtree --demuxer y4m --frames {video.SourceFrameCount} -o \"%~dp0Video{video.Extension}\" -";
+                vEncoder += $@"--aud --no-mbtree --demuxer y4m --frames {video.SourceFrameCount} -o ""%~dp0Video{video.Extension}"" -";
             }
 
             VideoEncoderScriptFile = $"{outputDir}Encode Video [{video.Codec}].cmd";
@@ -52,23 +52,23 @@ public class OutputScripts
     {
         if (audio.Enabled is true)
         {
-            string aPipe    = "avs2pipemod -wav=16bit \"%~dp0Script.avs\" | ",
+            string aPipe    = @"avs2pipemod -wav=16bit ""%~dp0Script.avs"" | ",
                    aEncoder = string.Empty;
 
             if (audio.Codec is "AAC-LC")
             {
                 aEncoder += $"qaac64 --abr {audio.Bitrate} --ignorelength --no-delay ";
-                aEncoder += $"-o \"%~dp0{fileInfo.FileNameOnly}{audio.Extension}\" - ";
+                aEncoder += $@"-o ""%~dp0{fileInfo.FileNameOnly}{audio.Extension}"" - ";
             }
             else if (audio.Codec is "AAC-HE")
             {
                 aEncoder += $"qaac64 --he{(audio.SourceChannels is "7.1" ? " --chanmask 0xff " : " ")}--abr {audio.Bitrate} --ignorelength ";
-                aEncoder += $"-o \"%~dp0{fileInfo.FileNameOnly}{audio.Extension}\" - ";
+                aEncoder += $@"-o ""%~dp0{fileInfo.FileNameOnly}{audio.Extension}"" - ";
             }
             else if (audio.Codec is "OPUS")
             {
                 aEncoder += $"opusenc --bitrate {audio.Bitrate} --ignorelength ";
-                aEncoder += $"- \"%~dp0{fileInfo.FileNameOnly}{audio.Extension}\"";
+                aEncoder += $@"- ""%~dp0{fileInfo.FileNameOnly}{audio.Extension}""";
             }
 
             AudioEncoderScriptFile = $"{outputDir}Encode Audio [{audio.Codec}].cmd";
@@ -80,8 +80,8 @@ public class OutputScripts
     {
         string containerTemplate = outputContainer switch
         {
-            "MP4" => $"mp4box $(video) $(audio) -new \"%~dp0{fileInfo.FileNameOnly}.mp4\"",
-            "MKV" => $"mkvmerge -o \"%~dp0{fileInfo.FileNameOnly}.mkv\" $(video) $(audio)",
+            "MP4" => $@"mp4box $(video) $(audio) -new ""%~dp0{fileInfo.FileNameOnly}.mp4""",
+            "MKV" => $@"mkvmerge -o ""%~dp0{fileInfo.FileNameOnly}.mkv"" $(video) $(audio)",
             _     => string.Empty
         };
         
@@ -89,13 +89,13 @@ public class OutputScripts
         {
             "MP4" => video.MuxOriginalVideo switch
             {
-                true  => $"-add \"{fileInfo.FileName}\"#video",
-                false => $"-add \"%~dp0Video{video.Extension}\":name="
+                true  => $@"-add ""{fileInfo.FileName}""#video",
+                false => $@"-add ""%~dp0Video{video.Extension}"":name="
             },
             "MKV" => video.MuxOriginalVideo switch
             {
-                true  => $"--no-audio \"{fileInfo.FileName}\"",
-                false => $"\"%~dp0Video{video.Extension}\""
+                true  => $@"--no-audio ""{fileInfo.FileName}""",
+                false => $@"""%~dp0Video{video.Extension}"""
             },
             _     => string.Empty
         };
@@ -104,12 +104,12 @@ public class OutputScripts
         {
             "MP4" => audio.Enabled switch
             {
-                true  => $"-add \"%~dp0{fileInfo.FileNameOnly}{audio.Extension}\":name=:lang={audio.LanguageCode}",
+                true  => $@"-add ""%~dp0{fileInfo.FileNameOnly}{audio.Extension}"":name=:lang={audio.LanguageCode}",
                 false => string.Empty
             },
             "MKV" => audio.Enabled switch
             {
-                true  => $"--language 0:{audio.LanguageCode} \"%~dp0{fileInfo.FileNameOnly}{audio.Extension}\"",
+                true  => $@"--language 0:{audio.LanguageCode} ""%~dp0{fileInfo.FileNameOnly}{audio.Extension}""",
                 false => string.Empty
             },
             _     => string.Empty
