3c169d94856089a186fc950e26c643f618dda15c
Shaylen <shaylenreddy42@gmail.com>
2022-12-13 16:58:25 +0200

    move the solution into a src folder

--- build.cmd
+++ build.cmd
@@ -1,50 +1,54 @@
 @ECHO off
 
-@CD "%~dp0"
+CD "%~dp0"
 
-@ECHO Run CMake
-@ECHO.
-cmake -G "Visual Studio 17" -S . -B build
+ECHO Run CMake
+ECHO.
+cmake -S . -B build
 
-@ECHO.
-@ECHO Restore Solution
-@ECHO.
+CD "%~dp0/src"
+
+ECHO.
+ECHO Restore Solution
+ECHO.
 dotnet restore
 
-@ECHO.
-@ECHO Build Solution
-@ECHO.
+ECHO.
+ECHO Build Solution
+ECHO.
 dotnet build -c Debug
 
-@ECHO.
-@ECHO Clean Older Tests If They Exist
-@ECHO.
+ECHO.
+ECHO Clean Older Tests If They Exist
+ECHO.
 IF EXIST SimpleAVSGeneratorCore.Tests\TestResults (
-	@RD SimpleAVSGeneratorCore.Tests\TestResults /S /Q
+	RD SimpleAVSGeneratorCore.Tests\TestResults /S /Q
 )
 
-@ECHO Run Unit Tests 
-@ECHO.
+ECHO Run Unit Tests 
+ECHO.
 dotnet test -c Debug --collect:"XPlat Code Coverage"
 
-@ECHO.
-@ECHO Restore Local Tools
-@ECHO.
+ECHO.
+ECHO Restore Local Tools
+ECHO.
 dotnet tool restore
 
-@ECHO.
-@ECHO Generate HTML Code Coverage Report
-@ECHO.
-dotnet tool run reportgenerator -reports:"SimpleAVSGeneratorCore.Tests\TestResults\*\*.xml" -reporttypes:HtmlInline_AzurePipelines -targetDir:"CodeCoverage"
+ECHO.
+ECHO Generate HTML Code Coverage Report
+ECHO.
+dotnet tool run reportgenerator -reports:"SimpleAVSGeneratorCore.Tests\TestResults\*\*.xml" -reporttypes:HtmlInline_AzurePipelines -targetDir:"..\CodeCoverage"
 
-@ECHO.
-@ECHO Publish the Final Executable
-@ECHO.
-dotnet publish SimpleAVSGenerator\SimpleAVSGenerator.csproj -c Release -o publish
+ECHO.
+ECHO Publish the Final Executable
+ECHO.
+dotnet publish SimpleAVSGenerator\SimpleAVSGenerator.csproj -c Release -o "..\publish"
 
-@ECHO.
-@ECHO Cleanup
-@ECHO.
-@RD build /S /Q
+CD ..
 
-@PAUSE
+ECHO.
+ECHO Cleanup
+ECHO.
+RD build /S /Q
+
+PAUSE
