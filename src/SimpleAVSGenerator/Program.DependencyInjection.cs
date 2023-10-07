d75aff58866fb4f606cc746d584919dee002268a
Shaylen Reddy <shaylenreddy42@gmail.com>
2023-10-07 21:24:55 +0200

    split the program class across two files using partial classes
    dependency injection moves into its own file to restore program.cs to its original state, keeping it clean

---
+++ src/SimpleAVSGenerator/Program.DependencyInjection.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using SimpleAVSGeneratorCore.Services;
+using SimpleAVSGeneratorCore.Support;
+
+namespace SimpleAVSGenerator;
+
+internal static partial class Program
+{
+    private static IServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<IConfiguration>(services =>
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Application.StartupPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        });
+
+        services.AddLogging(configure =>
+        {
+            var loggerConfiguration =
+                new LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Async(configure =>
+                        configure.File(
+                            path: Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Simple AVS Generator", "simpleavsgenerator-logs-.txt"),
+                            outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level}] <{SourceContext}> {NewLine}{Message:lj} {NewLine}{Exception}{NewLine}",
+                            rollingInterval: RollingInterval.Day,
+                            retainedFileTimeLimit: TimeSpan.FromDays(14)))
+                    .CreateLogger();
+
+            configure.AddSerilog(loggerConfiguration);
+        });
+
+        services.AddSingleton<MainForm>();
+
+        services.AddSingleton<Extensions>();
+
+        services.AddSingleton<IFileWriterService, FileWriterService>();
+
+        services.AddSingleton<IInputFileHandlerService, InputFileHandlerService>();
+
+        return services.BuildServiceProvider();
+    }
+}
--- src/SimpleAVSGenerator/Program.cs
+++ src/SimpleAVSGenerator/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
-using Serilog;
-using SimpleAVSGeneratorCore.Services;
-using SimpleAVSGeneratorCore.Support;
 
 namespace SimpleAVSGenerator;
 
-internal static class Program
+internal static partial class Program
 {
     /// <summary>
     ///  The main entry point for the application.
@@ -20,45 +17,5 @@ internal static class Program
         var services = ConfigureServices();
 
         Application.Run(services.GetRequiredService<MainForm>());
-    }
-
-    private static IServiceProvider ConfigureServices()
-    {
-        var services = new ServiceCollection();
-
-        services.AddSingleton<IConfiguration>(services =>
-        {
-            return new ConfigurationBuilder()
-                .SetBasePath(Application.StartupPath)
-                .AddJsonFile("appsettings.json")
-                .Build();
-        });
-
-        services.AddLogging(configure =>
-        {
-            var loggerConfiguration =
-                new LoggerConfiguration()
-                    .MinimumLevel.Information()
-                    .Enrich.FromLogContext()
-                    .WriteTo.Async(configure =>
-                        configure.File(
-                            path: Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Simple AVS Generator", "simpleavsgenerator-logs-.txt"),
-                            outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level}] <{SourceContext}> {NewLine}{Message:lj} {NewLine}{Exception}{NewLine}",
-                            rollingInterval: RollingInterval.Day,
-                            retainedFileTimeLimit: TimeSpan.FromDays(14)))
-                    .CreateLogger();
-
-            configure.AddSerilog(loggerConfiguration);
-        });
-
-        services.AddSingleton<MainForm>();
-
-        services.AddSingleton<Extensions>();
-
-        services.AddSingleton<IFileWriterService, FileWriterService>();
-
-        services.AddSingleton<IInputFileHandlerService, InputFileHandlerService>();
-
-        return services.BuildServiceProvider();
     }
 }
